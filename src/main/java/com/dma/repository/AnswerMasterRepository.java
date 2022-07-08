d7eab5eb37bd7b0ffd2359716c18fb2148ff1a9a
abhimanyu <abhimanyu6280@gmail.com>
2022-07-08 12:13:38 +0530

    AnserMaterRepository interface added

---
+++ src/main/java/com/dma/repository/AnswerMasterRepository.java
@@ -0,0 +1,9 @@
+package com.dma.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dma.entities.AnswerMaster;
+
+public interface AnswerMasterRepository extends JpaRepository<AnswerMaster, Long> {
+
+}
