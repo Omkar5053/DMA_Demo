e6551be187a65a3603f61a3bdd2fb28f9e1a363e
Omkar5053 <omkar62006@gmail.com>
2022-07-20 12:12:35 +0530

    Pushed

--- src/main/java/com/dma/controller/AnswerMasterController.java
+++ src/main/java/com/dma/controller/AnswerMasterController.java
@@ -1,9 +1,12 @@
 package com.dma.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,12 @@ public class AnswerMasterController {
 
 	@Autowired
 	private AnswerMasterRepository answerMasterRepository;
+	
+	@GetMapping("/getAnswers")
+	public List<AnswerMaster> getAnswers()
+	{
+		return answerMasterRepository.findAll();
+	}
 	
 	@PostMapping("/addAnswer")
 	public ResponseEntity<String> addAnswer(@RequestBody AnswerMaster answer)
